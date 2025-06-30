90499b5a2eb63c9ec581a7ab5f955745dc3c8743
Nikolay Viskov <nik70a@mail.ru>
2025-06-30 13:28:35 +0300

    new files and update

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+import json
+
+
+def open_json(path: str) -> list:
+    """Функция, которая возвращает список словарей с транзакциями из JSON-файла."""
+    with open(path, "r", encoding="utf-8") as f:
+        try:
+            return json.load(f)
+        except json.JSONDecodeError:
+            return []
+        except Exception:
+            return []
+
+
+print(open_json("../data/operation.json"))
